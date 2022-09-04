bccce642dcc9c6eeba4805a1de2083c6f6cf7603
WXY <23144591@qq.com>
2022-09-04 20:01:56 +0800

    info Code04_AllTimesMinToMax

---
+++ class01/src/Code04_AllTimesMinToMax.java
@@ -0,0 +1,72 @@
+import org.w3c.dom.html.HTMLLegendElement;
+
+import java.util.Stack;
+
+/**
+ * @author : WXY
+ * @create : 2022-09-04 19:16
+ * @Info :
+ * 给定一个只包含正整数的数组arr，arr中任何一个子数组sub，一定都可以
+ * 计算出（sub累加和）*（sub中的最小值）是什么，那么所有子数组中，这个值最大是多少？
+ */
+public class Code04_AllTimesMinToMax {
+    public static int max1(int[] arr) {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                int minNum = Integer.MAX_VALUE;
+                int sum = 0;
+                for (int k = i; k <= j; k++) {
+                    sum += arr[k];
+                    minNum = Math.min(minNum, arr[k]);
+                }
+                max = Math.max(max, minNum * sum);
+            }
+        }
+        return max;
+    }
+
+    public static int max2(int[] arr) {
+        int size = arr.length;
+        int[] sums = new int[size];
+        sums[0] = arr[0];
+        for (int i = 1; i < size; i++) {
+            sums[i] = sums[i - 1] + arr[i];
+        }
+        int max = Integer.MIN_VALUE;
+        Stack<Integer> stack = new Stack<Integer>();
+        for (int i = 0; i < size; i++) {
+            while (!stack.isEmpty() && arr[stack.peek()] >= arr[i]) {
+                int j = stack.pop();
+                max = Math.max(max, (stack.isEmpty() ? sums[i - 1] : (sums[i - 1] - sums[stack.peek()])) * arr[j]);
+            }
+            stack.push(i);
+        }
+        while (!stack.isEmpty()) {
+            int j = stack.pop();
+            max = Math.max(max, (stack.isEmpty() ? sums[size - 1] : (sums[size - 1] - sums[stack.peek()])) * arr[j]);
+        }
+        return max;
+
+    }
+    public static int[] gerenareRondomArray() {
+        int[] arr = new int[(int) (Math.random() * 20) + 10];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * 101);
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int testTime = 10000;
+        System.out.println("test begin");
+        for (int i = 0; i < testTime; i++) {
+            int[] arr = gerenareRondomArray();
+            if (max1(arr) != max2(arr)) {
+                System.out.println("BUG BUG BUG ");
+            }
+        }
+        System.out.println("FINISH");
+
+    }
+}
