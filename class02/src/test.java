31bc8cbf03c8631405094f1f8fb3555ab9e0fa98
WXY007 <11437254+rookiewxy007@user.noreply.gitee.com>
2022-09-05 22:24:14 +0800

    info hello world

---
+++ class02/src/test.java
@@ -0,0 +1,10 @@
+/**
+ * @author : WXY
+ * @create : 2022-09-05 22:23
+ * @Info :
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
