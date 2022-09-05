bc705d7f1daac6cf7b897f97f8fbaf662b56061e
WXY007 <11437254+rookiewxy007@user.noreply.gitee.com>
2022-09-05 22:25:12 +0800

    info hello world

--- class02/src/test.java
+++ class02/src/test.java
@@ -6,5 +6,8 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
     }
 }
