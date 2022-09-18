a1a84297f62c6cc6ca6844bd8e50e8f3c920b159
WXY007 <11437254+rookiewxy007@user.noreply.gitee.com>
2022-09-18 14:55:38 +0800

    info

---
+++ trainingcamp001/src/test.java
@@ -0,0 +1,11 @@
+/**
+ * @author : WXY
+ * @create : 2022-09-18 14:54
+ * @Info :
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+
+    }
+}
