178c0928356fab5348e5889e73faf2399eba4f65
WXY007 <11437254+rookiewxy007@user.noreply.gitee.com>
2022-10-27 19:54:55 +0800

    info

---
+++ trainingcamp002/camp02_class08/src/maid.java
@@ -0,0 +1,10 @@
+/**
+ * @author : WXY
+ * @create : 2022-10-27 19:54
+ * @Info :
+ */
+public class maid {
+    public static void main(String[] args) {
+        System.out.println("sa;kdjf");
+    }
+}
